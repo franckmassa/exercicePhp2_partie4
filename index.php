39718d6424a9ea1ff836b03a4786d58491a4b7b7
massa <massafranck@gmail.com>
2018-07-29 21:21:25 +0200

    initial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice2</title>
+  </head>
+  <body>
+<?php
+//Déclaration de la fonction  qui retourne la chaine de caractères "hello"
+function characterChain(){
+  return 'Hello!';
+}
+//affichage de la chaine de caractères "hello"
+echo characterChain();
+ ?>
+  </body>
+</html>
