0b342ae8a1a0508a46baab696f1c02d0f1acac5a
massa <massafranck@gmail.com>
2018-07-29 23:14:06 +0200

    indentation

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>Exercice2</title>
-  </head>
-  <body>
-<?php
-//Déclaration de la fonction  qui retourne la chaine de caractères "hello"
-function characterChain(){
-  return 'Hello!';
-}
-//affichage de la chaine de caractères "hello"
-echo characterChain();
- ?>
-  </body>
+<head>
+  <meta charset="utf-8" />
+  <title>Exercice2</title>
+</head>
+<body>
+  <p>
+    <?php
+    //Déclaration de la fonction  qui retourne la chaine de caractères "hello"
+    function characterChain(){
+      return 'Hello!';
+    }
+    //affichage de la chaine de caractères "hello"
+    echo characterChain();
+    ?>
+  </p>
+</body>
 </html>
