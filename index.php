600f756af903d4682847d99ac7d46b4884afda65
massa <massafranck@gmail.com>
2018-08-01 21:40:13 +0200

    corrigé

--- index.php
+++ index.php
@@ -1,19 +1,18 @@
+<?php
+//on declare la fonction qui retourne une chaine de caracteres passer en parametre.
+function retunrString($str){
+  return $str;
+}
+ ?>
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8" />
-  <title>Exercice2</title>
-</head>
-<body>
-  <p>
-    <?php
-    //Déclaration de la fonction  qui retourne la chaine de caractères "hello"
-    function characterChain(){
-      return 'Hello!';
-    }
-    //affichage de la chaine de caractères "hello"
-    echo characterChain();
-    ?>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice2</title>
+  </head>
+  <body>
+    <p>
+    <?= retunrString('Bonjour je suis une chaine caracteres'); //on affiche le retour de la fonction et on donne une valeur a son parametre ?>
   </p>
-</body>
+  </body>
 </html>
